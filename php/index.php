a289d8ff91e835fa416c7ed7f2078f6041922cab
Boris Jacquot <lemon.add0@gmail.com>
2017-02-10 16:59:18 +0100

    Ajout de css

--- php/index.php
+++ php/index.php
@@ -3,9 +3,13 @@
 
 	<head>
 		<title>Accueil</title>
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 
 	<body>
+
+
 
 	</body>
 </html>
